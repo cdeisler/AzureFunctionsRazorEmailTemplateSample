e4147b7a98178f7b57c3202f3771fc8254fa5379
cdeisler <c33jd13s3l@gmail.com>
2022-11-01 09:42:17 -0400

    fixes

--- SendRazorTemplateEmail.cs
+++ SendRazorTemplateEmail.cs
@@ -25,29 +25,29 @@ namespace AzureFunctionsRazorEmailTemplateSample.Function
         [FunctionName("SendRazorTemplateEmail")]
         public async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
-            [SendGrid] IAsyncCollector<SendGridMessage> messageCollector, 
+            //[SendGrid] IAsyncCollector<SendGridMessage> messageCollector, 
             ILogger log, CancellationToken cancellationToken)
         {
             string name = req.Query["name"];
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            //string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            //dynamic data = JsonConvert.DeserializeObject(requestBody);
+            //name = name ?? data?.name;
 
-            var message = new SendGridMessage();
+            //var message = new SendGridMessage();
 
-            message.SetFrom("from@example.com");
-            message.AddTo("to@example.com");
-            message.SetSubject("Subject");
+            //message.SetFrom("from@example.com");
+            //message.AddTo("to@example.com");
+            //message.SetSubject("Subject");
 
             var model = new SampleEmailTemplateModel()
             {
                 Name = name
             };
             var htmlContent = await razorViewToStringRenderer.RenderViewToStringAsync(model);
-            message.AddContent(MimeType.Html, htmlContent);
+            //message.AddContent(MimeType.Html, htmlContent);
 
-            await messageCollector.AddAsync(message, cancellationToken);
+            //await messageCollector.AddAsync(message, cancellationToken);
 
             log.LogInformation("C# HTTP trigger function processed a request.");
             return new OkObjectResult(htmlContent);
